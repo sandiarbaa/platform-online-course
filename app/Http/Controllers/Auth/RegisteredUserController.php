622f8bae560e20a649ae47af7b612af538bc03fa
sandiarbaa <sandiarbaa@gmail.com>
2024-04-16 03:58:20 +0700

    2. Modifikasi Migration dan Model User

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,24 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'occupation' => ['required', 'string', 'max:255'],
+            'avatar' => ['required', 'image', 'mimes:png, jpg, jpeg'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        // proses upload file photo kepada projek laravel ini
+        if($request->hasFile('avatar')) { // jika request(form) tersebut menerima sebuah file dari input typenya file dan namenya avatar, maka
+            $avatarPath = $request->file('avatar')->store('avatars', 'public'); // dapatkan path avatar, lalu simpan ke folder avatars dan simpan secara public agar bisa diakses di projek laravel ini
+        } else { // kalau user lupa mengupload avatar, maka akan diisi dengan avatar default
+            $avatarPath = 'images/avatar-default.png';
+        }
+        
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'occupation' => $request->occupation,
+            'avatar' => $request->avatarPath,
             'password' => Hash::make($request->password),
         ]);
 
